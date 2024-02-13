3b475caf2ee25aac1c0e3fa61e1432414406f5b2
simonakomarovska <simonak399@gmail.com>
2024-02-13 14:00:18 +0100

    jokes

---
+++ script.js
@@ -0,0 +1,19 @@
+const p = document.querySelector("p");
+const btn = document.querySelector("button");
+const url = "https://api.chucknorris.io/jokes/random";
+
+
+// function getJoke(){
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data=>p.innerHTML = data.value);
+// }
+
+const getJoke = async () => {
+    const promise = await fetch("https://api.chucknorris.io/jokes/random")
+    const response = await promise.json()
+    return p.innerHTML = response.value
+}
+
+btn.addEventListener("click", getJoke);
+
